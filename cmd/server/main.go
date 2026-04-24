41a0021f918a5559e20b9120c5599e704c948efb
sangiriah <lisaosadchenko@gmail.com>
2026-04-24 23:46:30 +0300

    chore: server startup

---
+++ cmd/server/main.go
@@ -0,0 +1,11 @@
+package main
+
+import "github.com/labstack/echo/v5"
+
+func main(){
+	e := echo.New()
+
+	if err := e.Start(":1323"); err != nil {
+		e.Logger.Error("failed to start server", "error", err)
+	}
+}
--- go.mod
+++ go.mod
@@ -1,3 +1,5 @@
 module subscription-service-go
 
 go 1.26.1
+
+require github.com/labstack/echo/v5 v5.1.0 // indirect
