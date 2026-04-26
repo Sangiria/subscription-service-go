db23e57c62f9f121fc798402bba2c45f3ac8d8f5
sangiriah <lisaosadchenko@gmail.com>
2026-04-27 01:14:36 +0300

    feat: added connection to database

---
+++ internal/environment/load.go
@@ -0,0 +1,15 @@
+package environment
+
+import (
+	"log"
+
+	"github.com/joho/godotenv"
+)
+
+func LoadEnvVariables() {
+	err := godotenv.Load(".env")
+	
+	if err != nil {
+		log.Fatalf("Error loading .env file")
+	}
+}
