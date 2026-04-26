4a34cafc66ff05d5bae7c50970fc9e3274097bcb
sangiriah <lisaosadchenko@gmail.com>
2026-04-27 01:20:10 +0300

    fix: proper field name

--- internal/handlers/subscription.go
+++ internal/handlers/subscription.go
@@ -2,6 +2,9 @@ package handlers
 
 import "github.com/labstack/echo/v5"
 
+type Subscription struct {
+}
+
 func CreateSubscription(c *echo.Context) error {
 	return nil
 }
--- migrations/001_subscription.sql
+++ migrations/001_subscription.sql
@@ -2,15 +2,15 @@
 -- +goose StatementBegin
 CREATE TABLE subscriptions (
     id UUID PRIMARY KEY DEFAULT gen_random_uuid(),
-    subscription_name TEXT NOT NULL,
-    cost INTEGER NOT NULL,
+    service_name TEXT NOT NULL,
+    price INTEGER NOT NULL,
     user_id UUID NOT NULL,
     start_date DATE NOT NULL,
     end_date DATE
 );
 CREATE INDEX idx_subscriptions_user_period ON subscriptions (
     user_id,
-    subscription_name,
+    service_name,
     start_date
 );
 -- +goose StatementEnd
