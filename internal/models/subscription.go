6731b0da77421395d0430ff2bbc50de7482b8cd1
sangiriah <lisaosadchenko@gmail.com>
2026-04-27 19:50:40 +0300

    feat: added subscription creation

--- internal/models/subscription.go
+++ internal/models/subscription.go
@@ -1,11 +1,22 @@
 package models
 
-type Subscription struct {
+import "time"
+
+type SubscriptionReq struct {
 	ServiceName string 		`json:"service_name" create:"required"`
 	Price		int			`json:"price" create:"required,gt=0"`
 	UserId		string		`json:"user_id" create:"required,uuid"`
 	StartDate	string		`json:"start_date" create:"required,datetime=01-2006"`
 	EndDate		string		`json:"end_date,omitzero" create:"omitzero,datetime=01-2006"`
+}
+
+type Subscription struct {
+	Id			string			`json:"id" gorm:"primaryKey;type:uuid;default:gen_random_uuid()"`
+	ServiceName string 			`json:"service_name"`
+	Price		int				`json:"price"`
+	UserId		string			`json:"user_id"`
+	StartDate	time.Time		`json:"start_date"`
+	EndDate		*time.Time		`json:"end_date,omitzero"`
 }
 
 const (
