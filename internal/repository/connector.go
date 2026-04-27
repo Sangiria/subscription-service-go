9e44dbf79ee1b58ed10bd1297e52fb4d252e0966
sangiriah <lisaosadchenko@gmail.com>
2026-04-27 16:29:03 +0300

    fix: proper db_connection env variable

--- internal/repository/connector.go
+++ internal/repository/connector.go
@@ -12,7 +12,7 @@ var Db *gorm.DB
 
 func GetDBConnection() {
 	var err error
-	Db, err = gorm.Open(postgres.Open(os.Getenv("DB_CONNECTION")), &gorm.Config{})
+	Db, err = gorm.Open(postgres.Open(os.Getenv("GOOSE_DBSTRING")), &gorm.Config{})
 
 	if err != nil {
 		log.Fatalf("Failed to connect to database")
