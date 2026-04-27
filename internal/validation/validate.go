8fe1ccf884ce2cb6dfd97fcf39791b9f3ff0d055
sangiriah <lisaosadchenko@gmail.com>
2026-04-27 18:27:51 +0300

    fix: pointer passing + prettier error messages

--- internal/validation/validate.go
+++ internal/validation/validate.go
@@ -1,17 +1,33 @@
 package validation
 
 import (
+	"fmt"
+	"strings"
+
 	"github.com/go-playground/validator/v10"
 	"github.com/labstack/echo/v5"
 )
 
 func BindAndValidate(c *echo.Context, req interface{}, tag string) error {
     if err := c.Bind(req); err != nil {
-        return err
+        return fmt.Errorf("data parsing error")
     }
 
     v := validator.New()
     v.SetTagName(tag)
-    
-    return v.Struct(req)
+
+    err := v.Struct(req)
+    if err == nil {
+        return nil
+    }
+
+	if vErrors, ok := err.(validator.ValidationErrors); ok {
+        var errMsgs []string
+        for _, ve := range vErrors {
+            errMsgs = append(errMsgs, fmt.Sprintf("field %s: %s", ve.Field(), ve.Tag()))
+        }
+        return fmt.Errorf("%s", strings.Join(errMsgs, "; "))
+    }
+
+    return err
 }
