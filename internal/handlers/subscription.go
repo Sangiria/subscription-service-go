e0d5b9c67c15589f0ecc6a5c585e6206b961eb03
sangiriah <lisaosadchenko@gmail.com>
2026-04-27 13:06:50 +0300

    feat: added request struct

--- internal/handlers/subscription.go
+++ internal/handlers/subscription.go
@@ -1,8 +1,17 @@
 package handlers
 
-import "github.com/labstack/echo/v5"
+import (
+	"time"
+
+	"github.com/labstack/echo/v5"
+)
 
 type Subscription struct {
+	ServiceName string 		`json:"service_name"`
+	Price		int			`json:"price"`
+	UserId		string		`json:"user_id"`
+	StartDate	time.Time	`json:"start_date"`
+	EndDate		time.Time	`json:"end_date,omitzero"`
 }
 
 func CreateSubscription(c *echo.Context) error {
