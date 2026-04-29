3814036571250e8ef51417c3b9b6898de2d5dac7
sangiriah <lisaosadchenko@gmail.com>
2026-04-29 22:50:01 +0300

    feat: added UpdateSubscriptionTests slice

---
+++ internal/handlers/subscription_fixtures_test.go
@@ -0,0 +1,159 @@
+package handlers
+
+import (
+	"net/http"
+	"subscription-service-go/internal/models"
+
+	"github.com/google/uuid"
+	"github.com/stretchr/testify/mock"
+	"gorm.io/gorm"
+)
+
+type CreateTest struct {
+	name           string
+    input          models.SubscriptionCreateReq
+    setupMock      func(m *MockRepository)
+    expectedStatus int
+    expectedBody   string
+}
+
+type UpdateTest struct {
+	name           	string
+	paramID			string
+    input          	models.SubscriptionUpdateReq
+    setupMock      	func(m *MockRepository)
+    expectedStatus 	int
+    expectedBody   	string
+}
+
+var testUUID = uuid.New().String()
+
+var UpdateSubscriptionTests = []UpdateTest{
+	{
+        name: "success update",
+        paramID: testUUID,
+        input: models.SubscriptionUpdateReq{ServiceName: new("Netflix Premium"), Price: new(500)},
+        setupMock: func(m *MockRepository) {
+            m.On("Update", testUUID, mock.MatchedBy(func(fields map[string]any) bool {
+                return fields["service_name"] == "Netflix Premium" && fields["price"] == float64(500)
+            })).Return(&models.Subscription{ServiceName: "Netflix Premium"}, nil)
+        },
+        expectedStatus: http.StatusOK,
+        expectedBody: "Netflix Premium",
+    },
+	{
+        name: "invalid uuid format",
+        paramID: "not-a-uuid",
+        input: models.SubscriptionUpdateReq{ServiceName: new("New")},
+        setupMock: func(m *MockRepository) {},
+        expectedStatus: http.StatusBadRequest,
+        expectedBody: "Invalid UUID format",
+    },
+	{
+        name: "empty request body",
+        paramID: testUUID,
+        input: models.SubscriptionUpdateReq{},
+        setupMock: func(m *MockRepository) {},
+        expectedStatus: http.StatusBadRequest,
+        expectedBody: "Update failed",
+    },
+	{
+        name: "record not found",
+        paramID: testUUID,
+        input: models.SubscriptionUpdateReq{ServiceName: new("Ghost")},
+        setupMock: func(m *MockRepository) {
+            m.On("Update", testUUID, mock.Anything).Return(nil, gorm.ErrRecordNotFound)
+        },
+        expectedStatus: http.StatusNotFound,
+        expectedBody: "This subscription doesn't exist",
+    },
+	{
+        name: "internal server error",
+        paramID: testUUID,
+        input: models.SubscriptionUpdateReq{ServiceName: new("Error")},
+        setupMock: func(m *MockRepository) {
+            m.On("Update", testUUID, mock.Anything).Return(nil, gorm.ErrInvalidDB)
+        },
+        expectedStatus: http.StatusInternalServerError,
+        expectedBody: "Error updating subscription record",
+    },
+}
+
+var CreateSubscriptionTests = []CreateTest{
+	{
+        name: "success",
+        input: models.SubscriptionCreateReq{
+            ServiceName: "Netflix", Price: 300, 
+            UserId: testUUID, StartDate: "07-2023",
+        },
+        setupMock: func(m *MockRepository) {
+            m.On("Create", mock.Anything).Return(nil)
+        },
+        expectedStatus: http.StatusOK,
+        expectedBody: "Netflix",
+    },
+    {
+        name: "validation failed - negative price",
+        input: models.SubscriptionCreateReq{
+            ServiceName: "Netflix", Price: -1, 
+            UserId: testUUID, StartDate: "07-2023",
+        },
+        setupMock: func(m *MockRepository) {},
+        expectedStatus: http.StatusBadRequest,
+        expectedBody: "Validation failed",
+    },
+	{
+        name: "validation failed - empty name",
+        input: models.SubscriptionCreateReq{
+            ServiceName: "", Price: 300, 
+            UserId: testUUID, StartDate: "07-2023",
+        },
+        setupMock: func(m *MockRepository) {},
+        expectedStatus: http.StatusBadRequest,
+        expectedBody: "Validation failed",
+    },
+	{
+        name: "validation failed - invalid uuid",
+        input: models.SubscriptionCreateReq{
+            ServiceName: "Netflix", Price: 300, 
+            UserId: "string", StartDate: "07-2023",
+        },
+        setupMock: func(m *MockRepository) {},
+        expectedStatus: http.StatusBadRequest,
+        expectedBody: "Validation failed",
+    },
+	{
+        name: "validation failed - invalid date",
+        input: models.SubscriptionCreateReq{
+            ServiceName: "Netflix", Price: 300, 
+            UserId: testUUID, StartDate: "0791832023",
+        },
+        setupMock: func(m *MockRepository) {},
+        expectedStatus: http.StatusBadRequest,
+        expectedBody: "Validation failed",
+    },
+    {
+        name: "conflict",
+        input: models.SubscriptionCreateReq{
+            ServiceName: "Netflix", Price: 300, 
+            UserId: testUUID, StartDate: "07-2023",
+        },
+        setupMock: func(m *MockRepository) {
+            m.On("Create", mock.Anything).Return(gorm.ErrDuplicatedKey)
+        },
+        expectedStatus: http.StatusConflict,
+        expectedBody: "This subscription already exist",
+    },
+	{
+        name: "internal server error",
+        input: models.SubscriptionCreateReq{
+            ServiceName: "Netflix", Price: 300, 
+            UserId: testUUID, StartDate: "07-2023",
+        },
+        setupMock: func(m *MockRepository) {
+            m.On("Create", mock.Anything).Return(gorm.ErrInvalidDB)
+        },
+        expectedStatus: http.StatusInternalServerError,
+        expectedBody: "Couldn't create subscription record",
+    },
+}
--- internal/handlers/subscription_test.go
+++ internal/handlers/subscription_test.go
@@ -8,11 +8,9 @@ import (
 	"subscription-service-go/internal/models"
 	"testing"
 
-	"github.com/google/uuid"
 	"github.com/labstack/echo/v5"
 	"github.com/stretchr/testify/assert"
 	"github.com/stretchr/testify/mock"
-	"gorm.io/gorm"
 )
 
 type MockRepository struct {
@@ -24,90 +22,15 @@ func (m *MockRepository) Create(sub *models.Subscription) error {
     return args.Error(0)
 }
 
-func (m *MockRepository) Get(id string) (*models.Subscription, error)
-func (m *MockRepository) List(limit int, offest int) ([]models.Subscription, error)
-func (m *MockRepository) Delete(id string) error
-func (m *MockRepository) Update(id string, fields map[string]any) (*models.Subscription, error)
+func (m *MockRepository) Get(id string) (*models.Subscription, error) { return nil, nil }
+func (m *MockRepository) List(limit int, offest int) ([]models.Subscription, error) { return nil, nil }
+func (m *MockRepository) Delete(id string) error { return nil }
+func (m *MockRepository) Update(id string, fields map[string]any) (*models.Subscription, error) { return nil, nil }
 
 func TestCreateSubscription(t *testing.T) {
     e := echo.New()
 
-	testUUID := uuid.New().String()
-    
-    tests := []struct {
-        name           string
-        input          models.SubscriptionCreateReq
-        setupMock      func(m *MockRepository)
-        expectedStatus int
-        expectedBody   string
-    }{
-        {
-            name: "success",
-            input: models.SubscriptionCreateReq{
-                ServiceName: "Netflix", Price: 300, 
-                UserId: testUUID, StartDate: "07-2023",
-            },
-            setupMock: func(m *MockRepository) {
-                m.On("Create", mock.Anything).Return(nil)
-            },
-            expectedStatus: http.StatusOK,
-            expectedBody:   "Netflix",
-        },
-        {
-            name: "validation failed - negative price",
-            input: models.SubscriptionCreateReq{
-                ServiceName: "Netflix", Price: -1, 
-                UserId: testUUID, StartDate: "07-2023",
-            },
-            setupMock:      func(m *MockRepository) {},
-            expectedStatus: http.StatusBadRequest,
-            expectedBody:   "Validation failed",
-        },
-		{
-            name: "validation failed - empty name",
-            input: models.SubscriptionCreateReq{
-                ServiceName: "", Price: 300, 
-                UserId: testUUID, StartDate: "07-2023",
-            },
-            setupMock:      func(m *MockRepository) {},
-            expectedStatus: http.StatusBadRequest,
-            expectedBody:   "Validation failed",
-        },
-		{
-            name: "validation failed - invalid uuid",
-            input: models.SubscriptionCreateReq{
-                ServiceName: "Netflix", Price: 300, 
-                UserId: "string", StartDate: "07-2023",
-            },
-            setupMock:      func(m *MockRepository) {},
-            expectedStatus: http.StatusBadRequest,
-            expectedBody:   "Validation failed",
-        },
-		{
-            name: "validation failed - invalid date",
-            input: models.SubscriptionCreateReq{
-                ServiceName: "Netflix", Price: 300, 
-                UserId: testUUID, StartDate: "0791832023",
-            },
-            setupMock:      func(m *MockRepository) {},
-            expectedStatus: http.StatusBadRequest,
-            expectedBody:   "Validation failed",
-        },
-        {
-            name: "conflict",
-            input: models.SubscriptionCreateReq{
-                ServiceName: "Netflix", Price: 300, 
-                UserId: testUUID, StartDate: "07-2023",
-            },
-            setupMock: func(m *MockRepository) {
-                m.On("Create", mock.Anything).Return(gorm.ErrDuplicatedKey)
-            },
-            expectedStatus: http.StatusConflict,
-            expectedBody:   "Record already exist",
-        },
-    }
-
-    for _, tt := range tests {
+    for _, tt := range CreateSubscriptionTests {
         t.Run(tt.name, func(t *testing.T) {
             mockRepo := new(MockRepository)
             tt.setupMock(mockRepo)
@@ -119,10 +42,15 @@ func TestCreateSubscription(t *testing.T) {
             rec := httptest.NewRecorder()
             c := e.NewContext(req, rec)
 
-            h.CreateSubscription(c)
+            err := h.CreateSubscription(c)
+            if err != nil {
+                e.HTTPErrorHandler(c, err)
+            }
 
             assert.Equal(t, tt.expectedStatus, rec.Code)
             assert.Contains(t, rec.Body.String(), tt.expectedBody)
+
+            mockRepo.AssertExpectations(t)
         })
     }
 }
