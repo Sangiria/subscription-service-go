f8da270e013301ca90cdbbc3db334db12b613368
sangiriah <lisaosadchenko@gmail.com>
2026-04-29 15:49:52 +0300

    fix: pass when params aren't set

--- internal/utils/converter.go
+++ internal/utils/converter.go
@@ -1,5 +1,19 @@
 package utils
 
+import "strconv"
+
 func Ptr[T any](v T) *T {
     return &v
+}
+
+func ToInt(valStr string, defaultVal int) int {
+    if valStr == "" {
+        return defaultVal
+    }
+
+    val, err := strconv.Atoi(valStr)
+    if err != nil {
+        return -2
+    }
+    return val
 }
