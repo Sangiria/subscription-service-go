6805f1f858c0a5be8614f719fd9338190f4c2a5b
sangiriah <lisaosadchenko@gmail.com>
2026-05-01 17:46:57 +0300

    refactor: get config func

--- cmd/server/main.go
+++ cmd/server/main.go
@@ -3,14 +3,15 @@ package main
 import (
 	"log/slog"
 	"os"
+	"subscription-service-go/internal/config"
 	"subscription-service-go/internal/database"
 	"subscription-service-go/internal/environment"
 	"subscription-service-go/internal/handlers"
 	"subscription-service-go/internal/repository"
 	"subscription-service-go/internal/routes"
 
-	"github.com/labstack/echo/v5/middleware"
 	"github.com/labstack/echo/v5"
+	"github.com/labstack/echo/v5/middleware"
 )
 
 func main(){
@@ -21,33 +22,7 @@ func main(){
 	logger := slog.New(slog.NewJSONHandler(os.Stdout, nil))
 
 	e := echo.New()
-	
-	e.Use(middleware.RequestLoggerWithConfig(middleware.RequestLoggerConfig{
-    LogStatus:   true,
-    LogURI:      true,
-    LogMethod:   true,
-    LogLatency:  true,
-    HandleError: true,
-    LogValuesFunc: func(c *echo.Context, v middleware.RequestLoggerValues) error {
-        	if v.Error != nil {
-            	logger.Error("request failed",
-                	slog.String("method", v.Method),
-                	slog.String("uri", v.URI),
-                	slog.Int("status", v.Status),
-                	slog.String("err", v.Error.Error()),
-            	)
-        	} else {
-            	logger.Info("request processed",
-                	slog.String("method", v.Method),
-                	slog.String("uri", v.URI),
-                	slog.Int("status", v.Status),
-                	slog.Duration("latency", v.Latency),
-            	)
-        	}
-        	return nil
-    	},
-	}))
-
+	e.Use(middleware.RequestLoggerWithConfig(config.GetRequestLoggerConfig(logger)))
 	routes.InitSubscriptionRoutes(e, handlers.NewSubscriptionHandler(repo))
 
 	if err := e.Start(":1323"); err != nil {
---
+++ internal/config/logger.go
@@ -0,0 +1,35 @@
+package config
+
+import (
+	"log/slog"
+
+	"github.com/labstack/echo/v5"
+	"github.com/labstack/echo/v5/middleware"
+)
+
+func GetRequestLoggerConfig(logger *slog.Logger) middleware.RequestLoggerConfig {
+    return middleware.RequestLoggerConfig{
+        LogStatus:   true,
+        LogURI:      true,
+        LogMethod:   true,
+        LogLatency:  true,
+        HandleError: true,
+        LogValuesFunc: func(c *echo.Context, v middleware.RequestLoggerValues) error {
+            attrs := []slog.Attr{
+                slog.String("method", v.Method),
+                slog.String("uri", v.URI),
+                slog.Int("status", v.Status),
+            }
+
+            if v.Error != nil {
+                attrs = append(attrs, slog.String("err", v.Error.Error()))
+                logger.LogAttrs(c.Request().Context(), slog.LevelError, "request failed", attrs...)
+            } else {
+                attrs = append(attrs, slog.Duration("latency", v.Latency))
+                logger.LogAttrs(c.Request().Context(), slog.LevelInfo, "request processed", attrs...)
+            }
+
+            return nil
+        },
+    }
+}
